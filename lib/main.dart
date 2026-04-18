ad7cb297a36f7b6597a7f5b4d90fe424c67ed3d0
MKhalidHossain <compilefiller@gmail.com>
2026-04-18 09:58:01 +0600

    "Drive Status" everywhere

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Bighustle',
+      title: 'Drive Status',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
